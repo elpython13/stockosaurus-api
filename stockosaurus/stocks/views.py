8947b604bfa1e38a0cf7c49495cb098371b8bdcb
Max <Max.alex.sanderson@gmail.com>
2021-03-23 19:52:51 +0000

    rename home to stocks

---
+++ stockosaurus/stocks/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
