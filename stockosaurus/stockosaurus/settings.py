f79b5cb575dda45d5ed04ea985ea4f4f83b76910
Kristian Ellis <kristian.w.ellis@gmail.com>
2021-03-23 20:16:14 +0000

    db fix

--- stockosaurus/stockosaurus/settings.py
+++ stockosaurus/stockosaurus/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'stockosaurus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
 
         # insert postgresql admin details
