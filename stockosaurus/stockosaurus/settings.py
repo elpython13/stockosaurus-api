7a07277b065030fbb968c2005c350e6962a21ffd
Max <Max.alex.sanderson@gmail.com>
2021-03-24 08:10:33 +0000

    Switch to using postgres db, add docker compose, update readme

--- stockosaurus/stockosaurus/settings.py
+++ stockosaurus/stockosaurus/settings.py
@@ -75,10 +75,12 @@ WSGI_APPLICATION = 'stockosaurus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
-        # insert postgresql admin details
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stockosaurus-db',
+        'USER': 'dev-admin',
+        'PASSWORD': '1234!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
